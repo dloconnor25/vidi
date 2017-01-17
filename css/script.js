921e532eeba77f88f1da3e9524fced52dece6df3
Gio <jmjones769@gmail.com>
2017-01-17 10:54:05 -0500

    update

--- css/script.js
+++ css/script.js
@@ -1,23 +1,3 @@
-$(document).ready(function(){
-  //Displays & hides content based on user's country focus.
-  $('.amcharts-map-area-CN').click(function(){
-  $('div.china').slideToggle(function(){
-  });
-  });
-
-  $('.amcharts-map-area-BR').click(function(){
-  $('div.brazil').slideToggle(function(){
-
-  });
-  });
-  //Hides the documentation link & download links.
-  $('#chartdiv').click(function(){
-  $('.amcharts-export-menu, div.amcharts-chart-div a').hide(function(){
-
-  });
-  });
-});
-
 var map = AmCharts.makeChart( "chartdiv", {
 
   "type": "map",
@@ -63,3 +43,25 @@ var map = AmCharts.makeChart( "chartdiv", {
 //   // $.ajax({url:url, dataType:"jsonp"}).done(function (results) { console.log(results) } );
 // }
 // map.addListener("click", i_got_clicked);
+
+
+$(document).ready(function(){
+  //Displays & hides content based on user's country focus.
+  $('.amcharts-map-area-CN').click(function(){
+    $('div.china').show();
+    $('div.brazil').hide();
+    $('div.china').slideDown('slow');
+  });
+
+  $('.amcharts-map-area-BR').click(function(){
+  $('div.brazil').show();
+  $('div.china').hide();
+
+  });
+  //Hides the documentation link & download links.
+  $('#chartdiv').click(function(){
+  $('.amcharts-export-menu, div.amcharts-chart-div a').hide(function(){
+
+  });
+  });
+});
